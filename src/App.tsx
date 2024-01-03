2b351d5c797f4f9fa8b35c8af76698152982bd0e
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-01-02 23:37:37 -0500

    add basic gamepad check

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react";
+
 const App = () => {
-  return <div>Hello World</div>;
+	useEffect(() => {
+		const step = () => {
+			navigator.getGamepads().forEach(gamepad => {
+				gamepad?.buttons.forEach((button, i) => {
+					if (button.value > 0) console.log(`gamepad ${gamepad.id} button ${i} pressed`);
+				});
+			});
+
+			requestAnimationFrame(step);
+		};
+
+		requestAnimationFrame(step);
+
+		// setup gamepads
+		const onConnect = (e: GamepadEvent) => {
+			console.log(
+				'Gamepad connected at index %d: %s. %d buttons, %d axes.',
+				e.gamepad.index,
+				e.gamepad.id,
+				e.gamepad.buttons.length,
+				e.gamepad.axes.length,
+			);
+		};
+
+		window.addEventListener('gamepadconnected', onConnect);
+
+		return () => {
+			window.removeEventListener('gamepadconnected', onConnect);
+		};
+	}, []);
+
+	return <div>Hello World</div>;
 }
 
 export default App;
