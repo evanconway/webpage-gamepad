5e7101f52c17f6f797d0f79a206f5878026ae318
Evan Conway <evanconwaymusic@gmail.com>
2024-01-12 17:47:14 -0500

    add left/right/medium/heavy version of shoryu and hado

--- src/components/Moves.tsx
+++ src/components/Moves.tsx
@@ -1,12 +1,25 @@
 import { selectArcadeStickHistory } from "../state/arcadeStickHistorySlice";
 import { useAppSelector } from "../state/hooks";
-import { arcadeStickHistoryMultiMatch, hadoukenLight, shoryukenLight } from "../util/moves";
+import { arcadeStickHistoryMultiMatch, hadoukenLeftHeavy, hadoukenLeftLight, hadoukenLeftMedium, hadoukenRightHeavy, hadoukenRightLight, hadoukenRightMedium, shoryukenLeftHeavy, shoryukenLeftLight, shoryukenLeftMedium, shoryukenRightHeavy, shoryukenRightLight, shoryukenRightMedium } from "../util/moves";
 
 const Moves = () => {
     const arcadeStickHistory = useAppSelector(selectArcadeStickHistory);
 
-    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenLight)) return <div>Shoryuken Light</div>
-    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenLight)) return <div>Hadouken Light</div>
+    // shoryuken
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenRightLight)) return <div>Shoryuken Right Light</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenRightMedium)) return <div>Shoryuken Right Medium</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenRightHeavy)) return <div>Shoryuken Right Heavy</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenLeftLight)) return <div>Shoryuken Left Light</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenLeftMedium)) return <div>Shoryuken Left Medium</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, shoryukenLeftHeavy)) return <div>Shoryuken Left Heavy</div>
+
+    // hadouken
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenRightLight)) return <div>Hadouken Right Light</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenRightMedium)) return <div>Hadouken Right Medium</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenRightHeavy)) return <div>Hadouken Right Heavy</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenLeftLight)) return <div>Hadouken Left Light</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenLeftMedium)) return <div>Hadouken Left Medium</div>
+    if (arcadeStickHistoryMultiMatch(arcadeStickHistory, hadoukenLeftHeavy)) return <div>Hadouken Left Heavy</div>
 
     return null;
 };
--- src/util/moves.ts
+++ src/util/moves.ts
@@ -41,20 +41,68 @@ const step = (direction: Direction, buttons?: Button | Button[]) => {
     return { direction, ...getButtons(buttons) };
 };
 
+/*
+An array of steps define a valid input sequence for a move. A complete move
+definition is an array of these step arrays, each a slightly different but
+legal way of inputting the move.
+*/
+
 const createMove = (...moves: ArcadeStickState[][]) => {
     return moves;
 }
 
+const copyMove = (move: ArcadeStickState[][]) => move.map(steps => steps.map(step => ({...step})));
+
+const reverseMove = (move: ArcadeStickState[][]) => {
+    const copy = copyMove(move);
+    for (let i = 0; i < copy.length; i++) {
+        const steps = copy[i];
+        for (let k = 0; k < steps.length; k++) {
+            if (steps[k].direction === 1) steps[k].direction = 3;
+            else if (steps[k].direction === 3) steps[k].direction = 1;
+            else if (steps[k].direction === 4) steps[k].direction = 6;
+            else if (steps[k].direction === 6) steps[k].direction = 4;
+            else if (steps[k].direction === 7) steps[k].direction = 9;
+            else if (steps[k].direction === 9) steps[k].direction = 7;
+        }
+    }
+    return copy;
+};
+
+const copyMoveButtonToButton = (move: ArcadeStickState[][], button: Button, changeTo: Button) => {
+    const copy = copyMove(move);
+    for (let i = 0; i < copy.length; i++) {
+        const steps = copy[i];
+        for (let k = 0; k < steps.length; k++) {
+            if (steps[k][button] === true) {
+                steps[k][button] = false;
+                steps[k][changeTo] = true;
+            }
+        }
+    }
+    return copy;
+};
+
 // moves
 
-export const shoryukenLight = createMove(
+export const shoryukenRightLight = createMove(
     [step(6), step(2), step(3, 'punch1')],
     [step(6), step(2), step(3), step(3, 'punch1')],
     [step(6), step(5), step(2), step(3, 'punch1')],
     [step(6), step(5), step(2), step(3), step(3, 'punch1')],
 );
+export const shoryukenRightMedium = copyMoveButtonToButton(shoryukenRightLight, 'punch1', 'punch2');
+export const shoryukenRightHeavy = copyMoveButtonToButton(shoryukenRightLight, 'punch1', 'punch3');
+export const shoryukenLeftLight = reverseMove(shoryukenRightLight);
+export const shoryukenLeftMedium = reverseMove(shoryukenRightMedium);
+export const shoryukenLeftHeavy = reverseMove(shoryukenRightHeavy);
 
-export const hadoukenLight = createMove(
+export const hadoukenRightLight = createMove(
     [step(2), step(3), step(6, 'punch1')],
     [step(2), step(3), step(6), step(6, 'punch1')],
 );
+export const hadoukenRightMedium = copyMoveButtonToButton(hadoukenRightLight, 'punch1', 'punch2');
+export const hadoukenRightHeavy = copyMoveButtonToButton(hadoukenRightLight, 'punch1', 'punch3');
+export const hadoukenLeftLight = reverseMove(hadoukenRightLight);
+export const hadoukenLeftMedium = reverseMove(hadoukenRightMedium);
+export const hadoukenLeftHeavy = reverseMove(hadoukenRightHeavy);
