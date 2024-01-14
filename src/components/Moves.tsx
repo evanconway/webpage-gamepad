804c04c2c658bb56924408ab0b7720b0f520e24e
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-01-14 16:08:47 -0500

    rework move construction and checking

--- src/components/Moves.tsx
+++ src/components/Moves.tsx
@@ -1,25 +1,25 @@
 import { selectArcadeStickHistory } from "../state/arcadeStickHistorySlice";
 import { useAppSelector } from "../state/hooks";
-import { arcadeStickHistoryMatchMove, hadoukenHeavy, hadoukenLight, hadoukenMedium, shoryukenHeavy, shoryukenLight, shoryukenMedium } from "../util/moves";
+import { arcadeStickHistoryMatchMove, move214PH, move214PL, move214PM, move236PH, move236PL, move236PM, move421PH, move421PL, move421PM, move623PH, move623PL, move623PM } from "../util/moves";
 
 const Moves = () => {
     const arcadeStickHistory = useAppSelector(selectArcadeStickHistory);
 
     // shoryuken
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenLight)) return <div>Shoryuken Right Light</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenMedium)) return <div>Shoryuken Right Medium</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenHeavy)) return <div>Shoryuken Right Heavy</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenLight, 'horizontal')) return <div>Shoryuken Left Light</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenMedium, 'horizontal')) return <div>Shoryuken Left Medium</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, shoryukenHeavy, 'horizontal')) return <div>Shoryuken Left Heavy</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move623PL)) return <div>{move623PL.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move623PM)) return <div>{move623PM.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move623PH)) return <div>{move623PH.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move421PL)) return <div>{move421PL.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move421PM)) return <div>{move421PM.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move421PH)) return <div>{move421PH.name}</div>
 
     // hadouken
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenLight)) return <div>Hadouken Right Light</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenMedium)) return <div>Hadouken Right Medium</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenHeavy)) return <div>Hadouken Right Heavy</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenLight, 'horizontal')) return <div>Hadouken Left Light</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenMedium, 'horizontal')) return <div>Hadouken Left Medium</div>
-    if (arcadeStickHistoryMatchMove(arcadeStickHistory, hadoukenHeavy, 'horizontal')) return <div>Hadouken Left Heavy</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move236PL)) return <div>{move236PL.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move236PM)) return <div>{move236PM.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move236PH)) return <div>{move236PH.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move214PL)) return <div>{move214PL.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move214PM)) return <div>{move214PM.name}</div>
+    if (arcadeStickHistoryMatchMove(arcadeStickHistory, move214PH)) return <div>{move214PH.name}</div>
 
     return null;
 };
--- src/util/moves.ts
+++ src/util/moves.ts
@@ -5,30 +5,8 @@ const MAX_TIME_BETWEEN_INPUTS_MS = 200;
 
 type Inversion = 'vertical' | 'horizontal' | 'vertical&horizontal';
 
-const arcadeStickStatesEqual = (a: ArcadeStickState, b: ArcadeStickState, inversion?: Inversion) => {
-    if (a.direction === 5 && b.direction !== 5) return false;
-    else if (inversion === 'horizontal') {
-        if (a.direction === 1 && b.direction !== 3) return false;
-        if (a.direction === 3 && b.direction !== 1) return false;
-        if (a.direction === 4 && b.direction !== 6) return false;
-        if (a.direction === 6 && b.direction !== 4) return false;
-        if (a.direction === 7 && b.direction !== 9) return false;
-        if (a.direction === 9 && b.direction !== 7) return false;
-    } else if (inversion === 'vertical') {
-        if (a.direction === 1 && b.direction !== 7) return false;
-        if (a.direction === 7 && b.direction !== 1) return false;
-        if (a.direction === 2 && b.direction !== 8) return false;
-        if (a.direction === 8 && b.direction !== 2) return false;
-        if (a.direction === 3 && b.direction !== 9) return false;
-        if (a.direction === 9 && b.direction !== 3) return false;
-    } else if (inversion === 'vertical&horizontal') {
-        if (a.direction === 1 && b.direction !== 9) return false;
-        if (a.direction === 9 && b.direction !== 1) return false;
-        if (a.direction === 2 && b.direction !== 8) return false;
-        if (a.direction === 8 && b.direction !== 2) return false;
-        if (a.direction === 3 && b.direction !== 7) return false;
-        if (a.direction === 7 && b.direction !== 3) return false;
-    } else if (a.direction !== b.direction) return false;
+const arcadeStickStatesEqual = (a: ArcadeStickState, b: ArcadeStickState) => {
+    if (a.direction !== b.direction) return false;
     if (a.punch1 !== b.punch1) return false;
     if (a.punch2 !== b.punch2) return false;
     if (a.punch3 !== b.punch3) return false;
@@ -38,7 +16,7 @@ const arcadeStickStatesEqual = (a: ArcadeStickState, b: ArcadeStickState, invers
     return true;
 };
 
-const arcadeStickHistoryMatch = (history: ArcadeStickStateTimed[], move: ArcadeStickState[], inversion?: Inversion) => {
+const arcadeStickHistoryMatch = (history: ArcadeStickStateTimed[], move: ArcadeStickState[]) => {
     if (history.length < move.length) return false;
     const moveReversed = [...move].reverse();
     debugger;
@@ -48,14 +26,19 @@ const arcadeStickHistoryMatch = (history: ArcadeStickStateTimed[], move: ArcadeS
             const timeDiff = history[i - 1].timeMs - history[i].timeMs;
             if (timeDiff > MAX_TIME_BETWEEN_INPUTS_MS) return false;
         }
-        if (!arcadeStickStatesEqual(moveReversed[i], history[i], inversion)) return false;
+        if (!arcadeStickStatesEqual(moveReversed[i], history[i])) return false;
     }
     return true;
 };
 
-export const arcadeStickHistoryMatchMove = (history: ArcadeStickStateTimed[], move: ArcadeStickState[][], inversion?: Inversion) => {
-    for (let moveVersion = 0; moveVersion < move.length; moveVersion++) {
-        if (arcadeStickHistoryMatch(history, move[moveVersion], inversion)) return true;
+interface Move {
+    inputHistories: ArcadeStickState[][];
+    name: string,
+}
+
+export const arcadeStickHistoryMatchMove = (history: ArcadeStickStateTimed[], move: Move) => {
+    for (let moveVersion = 0; moveVersion < move.inputHistories.length; moveVersion++) {
+        if (arcadeStickHistoryMatch(history, move.inputHistories[moveVersion])) return true;
     }
     return false;
 };
@@ -80,16 +63,45 @@ definition is an array of these step arrays, each a slightly different but
 legal way of inputting the move.
 */
 
-const createMove = (...moves: ArcadeStickState[][]) => {
-    return moves;
+const createMove = (name: string, ...moves: ArcadeStickState[][]): Move => {
+    return {
+        name,
+        inputHistories: moves,
+    };
 }
 
-const copyMove = (move: ArcadeStickState[][]) => move.map(steps => steps.map(step => ({...step})));
+const copyInputHistories = (move: ArcadeStickState[][]) => move.map(steps => steps.map(step => ({...step})));
 
-const copyMoveButtonToButton = (move: ArcadeStickState[][], button: Button, changeTo: Button) => {
-    const copy = copyMove(move);
-    for (let i = 0; i < copy.length; i++) {
-        const steps = copy[i];
+const copyMoveDirectionChange = (move: Move, newName: string, inversion: Inversion): Move => {
+    const inputHistories = copyInputHistories(move.inputHistories);
+    for (let i = 0; i < inputHistories.length; i++) {
+        const steps = inputHistories[i];
+        for (let k = 0; k < steps.length; k++) {
+            const step = steps[k];
+            if (inversion === 'horizontal') {
+                if (step.direction === 1) step.direction = 3;
+                else if (step.direction === 4) step.direction = 6;
+                else if (step.direction === 7) step.direction = 9;
+                else if (step.direction === 3) step.direction = 1;
+                else if (step.direction === 6) step.direction = 4;
+                else if (step.direction === 9) step.direction = 7;
+            } else if (inversion === 'vertical') {
+                if (step.direction === 1) step.direction = 7;
+                else if (step.direction === 2) step.direction = 8;
+                else if (step.direction === 3) step.direction = 9;
+                else if (step.direction === 7) step.direction = 1;
+                else if (step.direction === 8) step.direction = 2;
+                else if (step.direction === 9) step.direction = 3;
+            }
+        }
+    }
+    return { name: newName, inputHistories };
+};
+
+const copyMoveButtonToButton = (move: Move, newName: string, button: Button, changeTo: Button): Move => {
+    const inputHistories = copyInputHistories(move.inputHistories);
+    for (let i = 0; i < inputHistories.length; i++) {
+        const steps = inputHistories[i];
         for (let k = 0; k < steps.length; k++) {
             if (steps[k][button] === true) {
                 steps[k][button] = false;
@@ -97,23 +109,31 @@ const copyMoveButtonToButton = (move: ArcadeStickState[][], button: Button, chan
             }
         }
     }
-    return copy;
+    return { name: newName, inputHistories };
 };
 
 // moves
 
-export const shoryukenLight = createMove(
+export const move623PL = createMove(
+    '623PL',
     [step(6), step(2), step(3, 'punch1')],
     [step(6), step(2), step(3), step(3, 'punch1')],
     [step(6), step(5), step(2), step(3, 'punch1')],
     [step(6), step(5), step(2), step(3), step(3, 'punch1')],
 );
-export const shoryukenMedium = copyMoveButtonToButton(shoryukenLight, 'punch1', 'punch2');
-export const shoryukenHeavy = copyMoveButtonToButton(shoryukenLight, 'punch1', 'punch3');
+export const move623PM = copyMoveButtonToButton(move623PL, '623PM', 'punch1', 'punch2');
+export const move623PH = copyMoveButtonToButton(move623PL, '623PH', 'punch1', 'punch3');
+export const move421PL = copyMoveDirectionChange(move623PL, '421PL', 'horizontal');
+export const move421PM = copyMoveDirectionChange(move623PM, '421PM', 'horizontal');
+export const move421PH = copyMoveDirectionChange(move623PH, '421PH', 'horizontal');
 
-export const hadoukenLight = createMove(
+export const move236PL = createMove(
+    '236PL',
     [step(2), step(3), step(6, 'punch1')],
     [step(2), step(3), step(6), step(6, 'punch1')],
 );
-export const hadoukenMedium = copyMoveButtonToButton(hadoukenLight, 'punch1', 'punch2');
-export const hadoukenHeavy = copyMoveButtonToButton(hadoukenLight, 'punch1', 'punch3');
+export const move236PM = copyMoveButtonToButton(move236PL, '236PM', 'punch1', 'punch2');
+export const move236PH = copyMoveButtonToButton(move236PL, '236PH', 'punch1', 'punch3');
+export const move214PL = copyMoveDirectionChange(move236PL, '214PL', 'horizontal');
+export const move214PM = copyMoveDirectionChange(move236PM, '214PM', 'horizontal');
+export const move214PH = copyMoveDirectionChange(move236PH, '214PH', 'horizontal');
